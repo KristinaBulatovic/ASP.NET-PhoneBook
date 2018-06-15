6519f3844e05fe2628b71c5dafe1fad79a4f2b73
Kristina Bulatović <bulatovic.tina95@gmail.com>
2018-06-15 16:13:29 +0200

    Add files via upload

---
+++ PhoneBookQuery.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE PhoneBook; 
+
+USE PhoneBook;
+
+CREATE TABLE "DataDirectory" (
+    "ID" int IDENTITY(1,1) PRIMARY KEY,
+    "FirstName" varchar(40) NOT NULL,
+    "Lastname" varchar(40) NOT NULL,
+	"Telephone Number" varchar(20) NOT NULL
+);
